1d8ad722d4fe0e31e34533d8b22e58b308d15c71
jdillon72 <jdillon@students.mchenry.edu>
2025-08-27 15:15:06 -0500

    Add files via upload
    Adding Python programs for conversion.

---
+++ knock-knock joke.py
@@ -0,0 +1,9 @@
+# Knock-Knock Joke Program
+print("Knock, Knock.")
+print("Who's there?")
+print("Pencil tip.")
+print("Pencil tip who?")
+print("Oh what's the point.")
+
+# The lines above run each string on a unique line, the one below runs the code from one extended line. Using new line \n.
+print("Knock, knock.\nWho's there?\nPencil tip.\nPencil tip who?\nOh what's the point.")

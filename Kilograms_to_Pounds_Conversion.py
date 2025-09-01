b2ae10fdb0554d0f3619687734d4ad26f8485324
jdillon72 <jdillon@students.mchenry.edu>
2025-09-01 13:34:27 -0500

    Add files via upload
    Uploading files from a series of week 2 assignments.

---
+++ Kilograms_to_Pounds_Conversion.py
@@ -0,0 +1,22 @@
+# Formula conversion code
+
+kilo = 1
+kilo_to_pound = kilo * 2.205
+
+# Define variables for centimeter values
+km_value_1 = 10
+km_value_2 = 20
+km_value_3 = 30
+km_value_4 = 40
+
+# Results from value conersions:
+result_1 = str(km_value_1 * kilo_to_pound)
+result_2 = str(km_value_2 * kilo_to_pound)
+result_3 = str(km_value_3 * kilo_to_pound)
+result_4 = str(km_value_4 * kilo_to_pound)
+
+# Printed results.
+print(str(km_value_1) + " Kg in pounds is " + result_1 +" lbs.")
+print(str(km_value_2) + " Kg in pounds is " + result_2 +" lbs.")
+print(str(km_value_3) + " Kg in pounds is " + result_3 +" lbs.")
+print(str(km_value_4) + " Kg in pounds is " + result_3 +" lbs.")
---
+++ Python_Comments.py
@@ -0,0 +1,12 @@
+""" Type of vehicle still needs to be chosen. Are we describing a plane, train, or automobile?"""
+# Describing tank size of particular vehicle.
+tank_size = 15
+# Define the total distance covered
+distance = 600
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result
+print("Miles per gallon:", mpg)
+
+#TODO Pick class of vehicle.
+#TODO Decide if anything else needs to effect mpg becase of vehicle class.

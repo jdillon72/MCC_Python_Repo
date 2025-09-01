dfcf9d9e8a1b7c113c23a7fc3e2f387e23d17785
jdillon72 <jdillon@students.mchenry.edu>
2025-09-01 13:41:57 -0500

    Add files via upload
    Again, uploading files from week 2 assignments.

---
+++ Python_Data_Types.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 12)
+
+# Integer
+y = -25
+print("Integer:", y)
+
+# Float
+z = 1.25
+print("Float:", z)
+
+# String
+name = "Matt"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
